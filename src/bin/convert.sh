055e702f7e17cfcf1560decde7b6539c032d1ca2
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-06-21 11:26:25 -0400

    adding this

---
+++ src/bin/convert.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+input_file="$1"; shift
+ratio=640
+wanted_extension="mp4"
+
+bn_input_file=$( basename -- "$input_file")
+
+extension="${bn_input_file##*.}"
+filename="${bn_input_file%.*}"
+function debug {
+	date
+	echo File "$bn_input_file"
+	echo Extension "$extension"
+	echo filename "$filename"
+	echo "Output file" $output_file
+}
+
+output_file="${filename}__${ratio}.${wanted_extension}"
+debug
+
+if [[ -e "$output_file" ]]
+then
+	>&2 echo "Error: $output_file already exists..."
+	exit
+fi
+
+ffmpeg -i "$input_file" -vf scale=${ratio}:-1 "$output_file"
+date
