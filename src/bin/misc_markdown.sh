b3ed1ca2a4ed852cdcd77b66343e6a4a9f528039
Martin VanWinkle III <mvanwinkle@ias.edu>
2023-10-19 06:39:12 -0400

    progress

--- src/bin/misc_markdown.sh
+++ src/bin/misc_markdown.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Outputs a comment in markdown; I believe I was using this
+# as part of automatically generating markdown and just
+# didn't delete it.
+
 function markdown_comment
 {
 	local comment="$1"; shift
