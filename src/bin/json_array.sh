a247a5322150885b520cc468d6aaed1ce7e70b0d
Martin VanWinkle III <mvanwinkle@ias.edu>
2024-09-26 12:02:46 -0400

    progress

--- src/bin/json_array.sh
+++ src/bin/json_array.sh
@@ -22,4 +22,37 @@ function test_array
 	declare -p the_array
 }
 
-test_array
+function create_associative_array_from_json_string
+{
+	local -n aarr="$1" ; shift
+	local json_string="$1" ; shift
+
+	local key
+
+	local count=0
+	for entry in $( printf "%s" "$json_string" | jq 'to_entries[] | .key, .value' )
+	do
+		if [[ $((count%2)) == 0 ]]
+		then
+			key=$( printf "%s" "$entry" | jq --raw-output )
+		else
+			aarr["$key"]=$( printf "%s" "$entry" | jq --raw-output )
+		fi
+		count=$((count+1))
+	done
+
+
+}
+
+function test_associative_array
+{
+	local the_associative_array
+	declare -A the_associative_array
+	create_associative_array_from_json_string the_associative_array '{ "a":"1","b":"2"}'
+
+	declare -p the_associative_array
+}
+
+# test_array
+
+test_associative_array
